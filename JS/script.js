83928a9e0a1a8c0e545059e958d13b8054db627d
LucydDev <luqsy666@gmail.com>
2024-11-21 15:33:56 -0300

    MediaQueries V.4

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('open');
+}
+
 function toggleSubmenu(element) {
     // Verifica si el tamaño de pantalla es menor o igual a 600px
     if (window.innerWidth <= 600) {
